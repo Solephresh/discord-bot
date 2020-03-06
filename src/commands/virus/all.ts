1d49261fa5103102ac51d821addc24885b479519
EliteDaMyth <elitemythyt@gmail.com>
2020-03-06 11:31:42 +0530

    Update all.ts

--- src/commands/virus/all.ts
+++ src/commands/virus/all.ts
@@ -1,23 +1,54 @@
-import { Command, Argument } from 'discord-akairo';
-import { Message, TextChannel, MessageEmbed } from 'discord.js';
-import { promisify } from 'util';
+import {
+    Command,
+    Argument
+} from 'discord-akairo';
+import {
+    Message,
+    TextChannel,
+    MessageEmbed
+} from 'discord.js';
+import {
+    promisify
+} from 'util';
 import * as pidusage from 'pidusage';
-import { freemem, totalmem } from 'os';
+import {
+    freemem,
+    totalmem
+} from 'os';
 
 export default class extends Command {
-  constructor() {
-    super('all', {
-      aliases: ['all'],
-      category: 'virus',
-      description: 'Get all statistics of COVID-19'
-    });
-  }
+    constructor() {
+        super('all', {
+            aliases: ['all'],
+            category: 'virus',
+            description: 'Get all statistics of COVID-19'
+        });
+    }
 
-  public async exec(message: Message, args: any) {
-    const res = await require('node-fetch')('https://corona.lmao.ninja/all');
-    const data = await res.json();
+    public async exec(message: Message, args: any) {
+        const res = await require('node-fetch')('https://corona.lmao.ninja/all');
+        const data = await res.json();
 
-    message.channel.send(`**COVID-19** has **${data.cases}** cases, **${data.deaths}** deaths, and **${data.recovered}** who have recovered.`);
-  }
+        message.channel.send(
+            new MessageEmbed()
+            .setColor(this.client.color)
+            .setAuthor(`COVID-19 Global`)
+            .setFooter('COVID-19 realtime statistics')
+            .addFields([{
+                    name: 'Cases',
+                    value: country.cases,
+                    inline: true
+                },
+                {
+                    name: 'Deaths',
+                    value: country.deaths,
+                    inline: true
+                },
+                {
+                    name: 'Recovered',
+                    value: country.recovered,
+                    inline: true
+                },
+            ]));
+    }
 }
-
